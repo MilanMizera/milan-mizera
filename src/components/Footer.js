a2d1ba4aa49f05e6ba09577f94962635175ce77e
Milan Mizera <milan.mizera2323@gmail.com>
2023-08-21 00:07:36 +0200

    header and footer

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import "./footer.css"
+
+export const Footer = () => {
+
+
+
+  return (
+    <div className="footer-section">
+        
+        <p className="footer-text">Milan Mizera &copy; {new Date().getFullYear()}. Všechna práva vyhrazena.</p>
+        
+        
+        
+        </div>
+  )
+}
+
+export default Footer
---
+++ src/pages/Home.js
@@ -0,0 +1,9 @@
+
+
+export const Home = () => {
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home

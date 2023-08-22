64e071943c7dcfd23df57f51b0dc97b531369d06
Milan Mizera <milan.mizera2323@gmail.com>
2023-08-22 15:24:00 +0200

    test flexbox

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,27 @@
+import "./home.css"
 
+import mainImg from "../images/main-photo.jpg"
 
 export const Home = () => {
+
+
   return (
-    <div>Home</div>
+
+    <div className="Home-wrapper">
+
+      <div className="welcome-section-row">
+        <div className="col-main-text">
+        <h1>Dobrý den, jsem juniorní vývojař v JS</h1>
+        <p>Programování se věnuji již dva roky</p>
+        </div>
+        <div className="col-main-img">
+        <img className="main-img" src={mainImg}></img>
+        </div>
+      </div>
+
+
+
+    </div>
   )
 }
 

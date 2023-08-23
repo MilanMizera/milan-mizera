183462bb7d5dc40238eb4a646b63335f06991c57
Milan Mizera <milan.mizera2323@gmail.com>
2023-08-24 00:28:06 +0200

    sekce s čím pracuji + rozdělaná sekce s iframe a src code

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,11 @@
 import "./home.css"
 import { TbBrandJavascript } from "react-icons/tb";
 import mainImg from "../images/main-photo.jpg"
+/*icons*/
+import { FaGitSquare, FaReact, FaNpm } from "react-icons/fa";
+import { BsGithub, BsFiletypeHtml, BsFiletypeCss } from "react-icons/bs";
+import { DiCode } from "react-icons/di";
+
 
 export const Home = () => {
 
@@ -13,11 +18,40 @@ export const Home = () => {
         <div className="col-main-title">
           <h1>Dobrý den, jsem juniorní vývojař v JS</h1>
           <p>Programování se věnuji již dva roky</p>
-          <div> <TbBrandJavascript className="js-icon"></TbBrandJavascript></div>
+          <div>    <DiCode className="js-icon" ></DiCode></div>
+
         </div>
+
         <div className="col-main-img">
           <img className="main-img" src={mainImg}></img>
         </div>
+      </div>
+
+
+      <div className="with-i-work-icons-section">
+        <div className="col-with-i-work-text">
+          <h2 className="with-i-work-text">S čím aktivně pracuji ?</h2>
+        </div>
+
+        <div className="col-with-i-work-icons">
+          <FaGitSquare className="icon-1"></FaGitSquare>
+          <BsGithub className="icon-1"></BsGithub>
+          <FaReact className="icon-1"></FaReact>
+          <TbBrandJavascript className="icon-1"></TbBrandJavascript>
+          <BsFiletypeHtml className="icon-1"></BsFiletypeHtml>
+          <BsFiletypeCss className="icon-1"></BsFiletypeCss>
+          <FaNpm className="icon-1"></FaNpm>
+        </div>
+      </div>
+
+      <div className="my-work-section">
+        <div className="col-my-work-iframe"></div>
+        <iframe src="https://moje-ukoly.netlify.app/"></iframe>
+      </div>
+
+      <div className="col-my-work-src-code">
+
+
       </div>
 
 
